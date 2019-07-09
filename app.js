3a27ec8662e0f3327e183feb617c89ec5f59cd0a
YihsuanChen <yihsuanchen@kkbox.com>
2019-07-09 18:14:28 +0800

    Indentation

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const app = express();
 
 // GET Method
 app.get('/',(req, res) => {
-  res.send('request /GET');
+    res.send('request /GET');
 });
 
 // POST Method
 app.post('/',(req, res) => {
-  res.send('request /POST');
+    res.send('request /POST');
 });
 
 // PUT Method
 app.put('/',(req, res) => {
-  res.send('request /PUT');
+    res.send('request /PUT');
 });
 
 // DELETE Method
 app.delete('/',(req, res) => {
-  res.send('request /DELETE');
+    res.send('request /DELETE');
 });
 
 module.exports = app;
