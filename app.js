00aaa862f02b04c4bb22a3775b46f8ff89db342a
YihsuanChen <yihsuanchen@kkbox.com>
2019-07-10 12:32:16 +0800

    Adopt lowdb

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const app = express();
 
+// lowdb
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
 // GET Method
-app.get('/',(req, res) => {
-    res.status(200).send({ status: { type: 'ok', message: 'request /GET' }, data: null });
-});
+app.get('/:territory',(req, res) => {
+	const territory = req.params.territory;
+	const account = db.get('accounts').find({'territory': territory}).value();
 
-// POST Method
-app.post('/',(req, res) => {
-    res.status(200).send({ status: { type: 'ok', message: 'request /POST' }, data: null });
-});
-
-// PUT Method
-app.put('/',(req, res) => {
-    res.status(200).send({ status: { type: 'ok', message: 'request /PUT' }, data: null });
-});
-
-// DELETE Method
-app.delete('/',(req, res) => {
-    res.status(200).send({ status: { type: 'ok', message: 'request /DELETE' }, data: null });
+	if (account === undefined) {
+        res.status(500).send({ status: { type: 'error', message: 'Nonexistent.' }, data: null });
+    } else {
+    	res.status(200).send({ status: { type: 'ok', message: '' }, data: account });
+    }
 });
 
 /**
